3d4aa855fb3e53b3666eba8c2c311a8e6e0792c4
WaterCoFire <117800515+WaterCoFire@users.noreply.github.com>
2025-03-01 15:43:08 +0800

    another test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!111");
+        System.out.println("Hello world!");
+        System.out.println("111");
     }
 }
