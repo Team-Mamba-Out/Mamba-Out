22f427fc75d0e3941c49ae9a6de39464511cef5b
Jaim <majiangtao868@gmail.com>
2025-03-03 20:11:40 +0800

    Add some useful methods.

---
+++ src/main/java/org/mamba/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.mamba.Exception;
+
+import org.example.entity.Result;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Result hanleException(Exception ex){
+        ex.printStackTrace();
+        return Result.error(StringUtils.hasLength(ex.getMessage())? ex.getMessage() : "操作失败");
+    }
+}
