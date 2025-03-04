c11f5d0b18e8c6314a570e63f0e96c5d7fed8cce
shunyao wang <wsy46366277@gmail.com>
2025-03-05 00:16:41 +0800

    Add some functions

--- src/main/java/org/mamba/mapper/MessageMapper.java
+++ src/main/java/org/mamba/mapper/MessageMapper.java
@@ -1,12 +1,26 @@
 package org.mamba.mapper;
 
 import org.apache.ibatis.annotations.*;
+import org.mamba.entity.Message;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+/**
+ * Mapper interface for database operations on the Message entity.
+ */
 @Mapper
 public interface MessageMapper {
 
+    /**
+     * Inserts a new message into the database.
+     *
+     * @param Uid the user ID
+     * @param title the title of the message
+     * @param text the text of the message
+     * @param createTime the creation time of the message
+     * @param isRead the read status of the message
+     */
     @Insert("INSERT INTO mamba.message (Uid, title, text, createTime, isRead) VALUES (#{Uid}, #{title}, #{text}, #{createTime}, #{isRead})")
     void createMessage(@Param("Uid") Integer Uid,
                        @Param("title") String title,
@@ -14,6 +28,20 @@ public interface MessageMapper {
                        @Param("createTime") LocalDateTime createTime,
                        @Param("isRead") boolean isRead);
 
+    /**
+     * Deletes a message from the database by its ID.
+     *
+     * @param id the ID of the message to delete
+     */
     @Delete("DELETE FROM mamba.message WHERE id = #{id}")
     void deleteMessage(@Param("id") Integer id);
+
+    /**
+     * Retrieves all messages for a given user ID.
+     *
+     * @param Uid the user ID
+     * @return a list of messages
+     */
+    @Select("SELECT * FROM mamba.message WHERE Uid = #{Uid}")
+    List<Message> getMessagesByUid(@Param("Uid") Integer Uid);
 }
--- src/main/java/org/mamba/service/MessageService.java
+++ src/main/java/org/mamba/service/MessageService.java
@@ -1,8 +1,37 @@
 package org.mamba.service;
 
-import java.time.LocalDateTime;
+import org.mamba.entity.Message;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Service interface for managing messages.
+ */
 public interface MessageService {
+    /**
+     * Creates a new message.
+     *
+     * @param Uid the user ID
+     * @param title the title of the message
+     * @param text the text of the message
+     * @param createTime the creation time of the message
+     * @param isRead the read status of the message
+     */
     void createMessage(Integer Uid, String title, String text, LocalDateTime createTime, boolean isRead);
+
+    /**
+     * Deletes a message by its ID.
+     *
+     * @param id the ID of the message to delete
+     */
     void deleteMessage(Integer id);
+
+    /**
+     * Retrieves all messages for a given user ID.
+     *
+     * @param Uid the user ID
+     * @return a list of messages
+     */
+    List<Message> getMessagesByUid(Integer Uid);
 }
