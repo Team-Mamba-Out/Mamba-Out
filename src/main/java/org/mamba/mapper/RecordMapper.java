b5e0bd1d4aebe6fff06fba62d9b59c0a7bfc564a
Jaim <majiangtao868@gmail.com>
2025-03-04 11:32:48 +0800

    The backend of RECORD and ROOM.

--- src/main/java/org/mamba/mapper/RecordMapper.java
+++ src/main/java/org/mamba/mapper/RecordMapper.java
@@ -1,110 +1,84 @@
 package org.mamba.mapper;
 
 import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.jdbc.SQL;
 import org.mamba.entity.Record;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface RecordMapper {
+
     /**
      * Obtains the record list based on the conditions given.
-     *
-     * @param roomId    the room id
-     * @param userId    the user id
-     * @param startTime the start time (the query result should be later than this)
-     * @param endTime   the end time (the query result should be earlier than this)
-     * @param pageSize  the size of each page
-     * @param offset    the offset
-     * @return the list of all the records satisfying the condition(s)
      */
-    @Select({
-            "<script>",
-            "select * from mamba.record where 1=1",
-            "<if test='id != null'>",
-            "and id = #{id}",
-            "</if>",
-            "<if test='roomId != null'>",
-            "and roomId = #{roomId}",
-            "</if>",
-            "<if test='userId != null'>",
-            "and userId = #{userId}",
-            "</if>",
-            "<if test='startTime != null'>",
-            "and startTime >= #{startTime}",
-            "</if>",
-            "<if test='endTime != null'>",
-            "and endTime <= #{endTime}",
-            "</if>",
-            "order by recordTime desc",
-            "<if test='pageSize != null'>",
-            "limit #{pageSize}",
-            "</if>",
-            "<if test='offset != null'>",
-            "offset #{offset}",
-            "</if>",
-            "</script>"
-    })
-    List<Record> getRecords(Integer id, Integer roomId, Integer userId, LocalDateTime startTime, LocalDateTime endTime, Integer pageSize, Integer offset);
+    @SelectProvider(type = RecordSqlBuilder.class, method = "buildGetRecordsSql")
+    List<Record> getRecords(@Param("id") Integer id,
+                            @Param("roomId") Integer roomId,
+                            @Param("userId") Integer userId,
+                            @Param("startTime") LocalDateTime startTime,
+                            @Param("endTime") LocalDateTime endTime,
+                            @Param("pageSize") Integer pageSize,
+                            @Param("offset") Integer offset);
 
     /**
      * Insert a new record.
-     *
-     * @param roomId     Room ID
-     * @param userId     User ID
-     * @param startTime  Start Time
-     * @param endTime    End Time
-     * @param recordTime Record TIme
      */
-    @Insert("insert into mamba.record(roomId, userId, startTime, endTime, recordTime) values (#{roomId}, #{userId}, #{startTime}, #{endTime}, #{recordTime})")
-    void createRecord(Integer roomId, Integer userId, LocalDateTime startTime, LocalDateTime endTime, LocalDateTime recordTime);
+    @Insert("INSERT INTO mamba.record (roomId, userId, startTime, endTime, recordTime) " +
+            "VALUES (#{roomId}, #{userId}, #{startTime}, #{endTime}, #{recordTime})")
+    void createRecord(@Param("roomId") Integer roomId,
+                      @Param("userId") Integer userId,
+                      @Param("startTime") LocalDateTime startTime,
+                      @Param("endTime") LocalDateTime endTime,
+                      @Param("recordTime") LocalDateTime recordTime);
 
     /**
      * Deletes the record specified by id.
-     *
-     * @param id the provided id
      */
-    @Delete("delete from mamba.record where id = #{id}")
-    void deleteRecordById(Integer id);
+    @Delete("DELETE FROM mamba.record WHERE id = #{id}")
+    void deleteRecordById(@Param("id") Integer id);
+
+    /**
+     * Static class to build SQL queries for the record table.
+     */
+    class RecordSqlBuilder {
+        public static String buildGetRecordsSql(Map<String, Object> params) {
+            SQL sql = new SQL() {{
+                SELECT("*");
+                FROM("mamba.record");
+                WHERE("1=1");
+
+                if (params.get("id") != null) {
+                    WHERE("id = #{id}");
+                }
+                if (params.get("roomId") != null) {
+                    WHERE("roomId = #{roomId}");
+                }
+                if (params.get("userId") != null) {
+                    WHERE("userId = #{userId}");
+                }
+                if (params.get("startTime") != null) {
+                    WHERE("startTime >= #{startTime}");
+                }
+                if (params.get("endTime") != null) {
+                    WHERE("endTime <= #{endTime}");
+                }
+                ORDER_BY("recordTime DESC");
+            }};
+
+            // Deal with LIMIT and OFFSET
+            String query = sql.toString();
+            if (params.get("pageSize") != null) {
+                if (params.get("offset") != null) {
+                    query += " LIMIT #{pageSize} OFFSET #{offset}";
+                } else {
+                    query += " LIMIT #{pageSize}";
+                }
+            }
+            return query;
+        }
+
+    }
 }
-
-
-///**
-// * Obtains the record specified by ID given.
-// *
-// * @param id the provided id
-// * @return the corresponding record, could be null
-// */
-//@Select("select * from mamba.record where id = #{id};")
-//Record getRecordById(int id);
-//
-///**
-// * Obtains the list of records specified by the User ID given.
-// *
-// * @param userId the provided User ID
-// * @return the corresponding record list
-// */
-//@Select("select * from mamba.record where userId = #{userId} order by recordTime desc;")
-//List<Record> getRecordsByUserId(int userId);
-//
-///**
-// * Obtains the list of records specified by the Room ID given.
-// *
-// * @param roomId the provided Room ID
-// * @return the corresponding record list
-// */
-//@Select("select * from mamba.record where roomId = #{roomId} order by recordTime desc;")
-//List<Record> getRecordsByRoomId(int roomId);
-//
-///**
-// * Obtains the list of records specified by the start time & end time range.
-// * Only returns records that have the start time later than the time provided
-// * and the end time earlier than the time provided.
-// *
-// * @param startTime the provided start time
-// * @param endTime   the provided end time
-// * @return the corresponding record list
-// */
-//@Select("select * from mamba.record where startTime >= #{startTime} and endTime <= #{endTime} order by recordTime desc;")
-//List<Record> getRecordsByTimeRange(LocalDateTime startTime, LocalDateTime endTime);
